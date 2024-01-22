38bff69cc46955cd51e651b8990a384581ce9b8e
Asian Himothy <evan.yang23@student.cart.org>
2024-01-22 09:45:18 -0800

    .

--- 1.js
+++ 1.js
@@ -1,12 +1,13 @@
 const numpattern = /\d+/;
 
 let sum = [];
+let realSum = [];
 
 let temp = [];
 let initial = false;
 let last = false;
 
-function firstNlast(str) {
+function firstNlast(str, real) {
     if (str.length == 0) return;
     for (let i= 0; i != str.length; i++ ) {
         if(str[i].match(numpattern)) {
@@ -22,12 +23,36 @@ function firstNlast(str) {
     }
     
     if (temp.length == 1) temp.push(initial);
+    
     initial = false
     last = false
    
     temp = temp.reduce((partialSum, a) => partialSum + a); // @https://stackoverflow.com/questions/1230233/how-to-find-the-sum-of-an-array-of-numbers
-    sum.push(temp)
-    temp = []
+
+    switch(real) {
+        case true:
+            realSum.push(temp)
+            break;
+
+        default: 
+            sum.push(temp)
+            break;
+    }
+
+    temp = [];
+}
+
+function convertToReal(str) {
+    str = str.replace('one', '1');
+    str = str.replace('two', '2');
+    str = str.replace('three', '3');
+    str = str.replace('four', '4');
+    str = str.replace('five', '5');
+    str = str.replace('six', '6');
+    str = str.replace('seven', '7');
+    str = str.replace('eight', '8');
+    str = str.replace('nine', '9');
+    return str;
 }
 
 const puzzleinput = document.getElementById('input').textContent;
@@ -40,8 +65,15 @@ for (let i = 0; i != linebreak.length; i++) {
     firstNlast(linebreak[i])
 }
 
-console.log("Sum Table: ", sum)
-
 sum = sum.reduce((partialSum, a) => Number(partialSum) + Number(a));
 
-console.log('Final Sum (Answer): '+sum)
+console.log('Part 1: Final Sum: '+sum)
+
+
+for (let i = 0; i != linebreak.length; i++) {
+    firstNlast(convertToReal(linebreak[i]), true)
+}
+
+realSum = realSum.reduce((partialSum, a) => Number(partialSum) + Number(a));
+
+console.log(realSum)
