4b6c32c092d9a0f935497c7de0baeae02f1c3d87
Asian Himothy <evan.yang23@student.cart.org>
2024-01-22 10:41:18 -0800

    .

--- 1.js
+++ 1.js
@@ -3,77 +3,71 @@ const numpattern = /\d+/;
 let sum = [];
 let realSum = [];
 
-let temp = [];
-let initial = false;
-let last = false;
+let initial, last;
 
-function firstNlast(str, real) {
-    if (str.length == 0) return;
-    for (let i= 0; i != str.length; i++ ) {
-        if(str[i].match(numpattern)) {
-            if (initial == false) { // if there is no initial value set 
-                initial = str[i];
-            } else if (last == false) { // if there is no last value set
-                last = true
-            } else if (last == true && temp.length == 2) { // if there is a last value set, remove it and add the new one
-                temp.pop()
-            };
-            temp.push(str[i])
-        };
+function getFirstAndLast(str, real) {
+    for (let i = 0; i != str.length; i++ ) {
+        switch (str[i].match(numpattern) == true) {
+            case true: 
+                if (!initial) {
+                    initial = str[i]
+                }
+                last = str[i]
+                console.log(last)
+        }
     }
-    
-    if (temp.length == 1) temp.push(initial);
-    
-    initial = false
-    last = false
-   
-    temp = temp.reduce((partialSum, a) => partialSum + a); // @https://stackoverflow.com/questions/1230233/how-to-find-the-sum-of-an-array-of-numbers
 
     switch(real) {
         case true:
-            realSum.push(temp)
+            realSum.push(initial + last)
             break;
 
         default: 
-            sum.push(temp)
+            sum.push(initial + last)
+            console.log(sum)
             break;
     }
 
-    temp = [];
+    initial, last = null, null
+}
+
+function getSum(input, real) {
+    input = input.split('\n')
+    input.pop() // necessary b/c splitting by \n gives us an extra line at the end.
+
+    switch (real) {
+        case true: 
+            for (let i = 0; i != input.length; i++) {
+                getFirstAndLast(convertToReal(input[i]), true)
+            }
+            realSum = realSum.reduce((partialSum, a) => Number(partialSum) + Number(a));
+            return realSum;
+            
+        default:
+            for (let i = 0; i != input.length; i++) {
+                getFirstAndLast(input[i])
+            }
+            sum = sum.reduce((partialSum, a) => Number(partialSum) + Number(a));
+            return sum;
+    }
 }
 
 function convertToReal(str) {
-    str = str.replace('one', '1');
-    str = str.replace('two', '2');
-    str = str.replace('three', '3');
-    str = str.replace('four', '4');
-    str = str.replace('five', '5');
-    str = str.replace('six', '6');
-    str = str.replace('seven', '7');
-    str = str.replace('eight', '8');
-    str = str.replace('nine', '9');
+    str = str.replaceAll('one', '1');
+    str = str.replaceAll('two', '2');
+    str = str.replaceAll('three', '3');
+    str = str.replaceAll('four', '4');
+    str = str.replaceAll('five', '5');
+    str = str.replaceAll('six', '6');
+    str = str.replaceAll('seven', '7');
+    str = str.replaceAll('eight', '8');
+    str = str.replaceAll('nine', '9');
     return str;
 }
 
 const puzzleinput = document.getElementById('input').textContent;
 
-const linebreak = puzzleinput.split("\n")
-
-linebreak.pop // gives us an extra blank line by accident.
-
-for (let i = 0; i != linebreak.length; i++) {
-    firstNlast(linebreak[i])
-}
-
-sum = sum.reduce((partialSum, a) => Number(partialSum) + Number(a));
-
-console.log('Part 1: Final Sum: '+sum)
+console.log('Part 1: Final Sum: '+ getSum(puzzleinput))
 
 
-for (let i = 0; i != linebreak.length; i++) {
-    firstNlast(convertToReal(linebreak[i]), true)
-}
-
-realSum = realSum.reduce((partialSum, a) => Number(partialSum) + Number(a));
-
-console.log(realSum)
+console.log('Part 2: Final Sum: '+ getSum(puzzleinput, true))
