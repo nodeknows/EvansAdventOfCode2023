69ec8b77ab64700f0c967abb258de2d1d4d0e704
Asian Himothy <evan.yang23@student.cart.org>
2024-01-25 10:41:47 -0800

    .

--- 1.js
+++ 1.js
@@ -1,73 +1,25 @@
-const numpattern = /\d+/;
+var puzput = document.getElementById('input').textContent;
 
-let sum = [];
-let realSum = [];
+// seperate our puzzle input per line
+puzput = puzput.split('\n')
+puzput.pop() // remove last item b/c it is an empty newline
 
-let initial, last;
+let s1 = 0;
 
-function getFirstAndLast(str, real) {
-    for (let i = 0; i != str.length; i++ ) {
-        switch (str[i].match(numpattern) == true) {
-            case true: 
-                if (!initial) {
-                    initial = str[i]
-                }
-                last = str[i]
-                console.log(last)
-        }
+function fnl(str, cnvrt) { // First And Last func
+
+    if (cnvrt) {
+        str.match(/one|two|three|four|five|six|seven|eight|nine/g)
     }
 
-    switch(real) {
-        case true:
-            realSum.push(initial + last)
-            break;
+    const n = str.match(/\d/g);
 
-        default: 
-            sum.push(initial + last)
-            console.log(sum)
-            break;
-    }
+    if (!n) return;
 
-    initial, last = null, null
+    if (n.length === 1) n.push(n[0]);
+
+    s1 += Number(n[0]+n[n.length-1])
 }
 
-function getSum(input, real) {
-    input = input.split('\n')
-    input.pop() // necessary b/c splitting by \n gives us an extra line at the end.
+puzput.forEach(fnl)
 
-    switch (real) {
-        case true: 
-            for (let i = 0; i != input.length; i++) {
-                getFirstAndLast(convertToReal(input[i]), true)
-            }
-            realSum = realSum.reduce((partialSum, a) => Number(partialSum) + Number(a));
-            return realSum;
-            
-        default:
-            for (let i = 0; i != input.length; i++) {
-                getFirstAndLast(input[i])
-            }
-            sum = sum.reduce((partialSum, a) => Number(partialSum) + Number(a));
-            return sum;
-    }
-}
-
-function convertToReal(str) {
-    str = str.replaceAll('one', '1');
-    str = str.replaceAll('two', '2');
-    str = str.replaceAll('three', '3');
-    str = str.replaceAll('four', '4');
-    str = str.replaceAll('five', '5');
-    str = str.replaceAll('six', '6');
-    str = str.replaceAll('seven', '7');
-    str = str.replaceAll('eight', '8');
-    str = str.replaceAll('nine', '9');
-    return str;
-}
-
-const puzzleinput = document.getElementById('input').textContent;
-
-console.log('Part 1: Final Sum: '+ getSum(puzzleinput))
-
-
-console.log('Part 2: Final Sum: '+ getSum(puzzleinput, true))
