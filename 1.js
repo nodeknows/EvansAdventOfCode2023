24653e758497ce59bb5855a2389f477ae88023a9
Asian Himothy <143831802+nodeknows@users.noreply.github.com>
2024-01-26 08:38:09 -0800

    .

--- 1.js
+++ 1.js
@@ -5,21 +5,62 @@ puzput = puzput.split('\n')
 puzput.pop() // remove last item b/c it is an empty newline
 
 let s1 = 0;
+let s2 = 0;
 
 function fnl(str, cnvrt) { // First And Last func
-
-    if (cnvrt) {
-        str.match(/one|two|three|four|five|six|seven|eight|nine/g)
-    }
-
     const n = str.match(/\d/g);
 
     if (!n) return;
 
+    if (cnvrt === true) {
+        var cnvrtabls = []
+        const re = /(?=(one|two|three|four|five|six|seven|eight|nine))/g;
+        cnvrtabls = Array.from(str.matchAll(re), x => x[1])
+        console.log(str)
+        console.log(cnvrtabls)
+    }
+
     if (n.length === 1) n.push(n[0]);
 
-    s1 += Number(n[0]+n[n.length-1])
+    /*
+
+function replaceAt(str, pattern, repl) {
+    return [str.replace(pattern, repl), str.match(pattern)]
+}
+
+let a = "zzz3twone1";
+let c = "";
+
+a.split('').forEach(char => {
+    c += char
+    let s = replaceAt(c, /one|two/g, 2)
+    c = s[0]
+    if (s[1]) {
+        console.log(s[1])
+    }
+})
+
+console.log(c)
+
+    */
+
+    switch (cnvrt) {
+        case true:
+            s2 += Number(n[0] + n[n.length - 1])
+            break;
+
+        default:
+            s1 += Number(n[0] + n[n.length - 1])
+            break;
+    }
+
 }
 
 puzput.forEach(fnl)
+console.log('Part 1 Solution: ' + s1)
 
+puzput.forEach(function (puz) {
+    fnl(puz, true)
+})
+
+console.log('Part 2 Solution: ' + s2)
