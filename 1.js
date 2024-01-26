efbc7a0b540aa51a6eafeaf7cb57dd7ac768a3e2
Asian Himothy <evan.yang23@student.cart.org>
2024-01-26 10:42:56 -0800

    .

--- 1.js
+++ 1.js
@@ -8,45 +8,47 @@ let s1 = 0;
 let s2 = 0;
 
 function fnl(str, cnvrt) { // First And Last func
-    const n = str.match(/\d/g);
-
-    if (!n) return;
+    let n = str.match(/\d/g);
 
     if (cnvrt === true) {
         var cnvrtabls = []
-        const re = /(?=(one|two|three|four|five|six|seven|eight|nine))/g;
-        cnvrtabls = Array.from(str.matchAll(re), x => x[1])
-        console.log(str)
-        console.log(cnvrtabls)
+
+        str = str.replace(/1/g, 'one');
+        str = str.replace(/2/g, 'two');
+        str = str.replace(/3/g, 'three');
+        str = str.replace(/4/g, 'four');
+        str = str.replace(/5/g, 'five');
+        str = str.replace(/6/g, 'six');
+        str = str.replace(/7/g, 'seven');
+        str = str.replace(/8/g, 'eight');
+        str = str.replace(/9/g, 'nine');
+
+        const re = /(?=(\one|two|three|four|five|six|seven|eight|nine))/g;
+        n = Array.from(str.matchAll(re), x => x[1])
+
+        for (let i = 0; i < n.length; i++) {
+            n[i] = n[i].replace(/one/g, '1')
+            n[i] = n[i].replace(/two/g, '2')
+            n[i] = n[i].replace(/three/g, '3')
+            n[i] = n[i].replace(/four/g, '4')
+            n[i] = n[i].replace(/five/g, '5')
+            n[i] = n[i].replace(/six/g, '6')
+            n[i] = n[i].replace(/seven/g, '7')
+            n[i] = n[i].replace(/eight/g, '8')
+            n[i] = n[i].replace(/nine/g, '9')
+        }
+
+        console.log(n)
     }
+
+    if (!n) return;
 
     if (n.length === 1) n.push(n[0]);
-
-    /*
-
-function replaceAt(str, pattern, repl) {
-    return [str.replace(pattern, repl), str.match(pattern)]
-}
-
-let a = "zzz3twone1";
-let c = "";
-
-a.split('').forEach(char => {
-    c += char
-    let s = replaceAt(c, /one|two/g, 2)
-    c = s[0]
-    if (s[1]) {
-        console.log(s[1])
-    }
-})
-
-console.log(c)
-
-    */
 
     switch (cnvrt) {
         case true:
             s2 += Number(n[0] + n[n.length - 1])
+            //console.log(s2, n[0], n[n.length - 1])
             break;
 
         default:
@@ -64,3 +66,27 @@ puzput.forEach(function (puz) {
 })
 
 console.log('Part 2 Solution: ' + s2)
+
+/*
+
+    function replaceAt(str, pattern, repl) {
+        return [str.replace(pattern, repl), str.match(pattern)]
+    }
+
+    let a = "zzz3twone1";
+    let c = "";
+    var sp = a.split('');
+
+    for (let i = 0; i != s.length-1; i++) {
+        c += sp[i]
+        let s = replaceAt(c, /two/g, 2)
+        s = replaceAt(c, /one/g, 1)
+        c = s[0]
+        if (s[1]) {
+            i -= s[1][0].length-1
+        }
+    }
+
+    console.log(c)
+
+    */
--- 2.js
+++ 2.js
@@ -1,16 +1,7 @@
-const pos = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green';
-const impos = 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red'
+/*
+let posid = pos.match(/\d+\:/g)[0]
 
-// 12 red cubes, 13 green cubes, and 14 blue cubes
-
-// sets are separated by ;
-
-// add up id's (later)
-
-let id = pos.match(/\d+\:/g)[0]
-
-console.log(id)
-console.log(id.slice(0, -1))
+console.log(posid.slice(0, -1))
 
 console.log(pos.match(/\d+ blue/g))
 console.log(pos.match(/\d+ red/g))
@@ -27,3 +18,48 @@ const address = '100 Queen St W, Toronto, ON M5H 2N2';
 const civicNumber = address
 
 console.log(civicNumber)
+*/
+
+/*
+
+KEY:
+
+index 1 is the id.
+
+; at the end means end of game.
+
+# before "blue," "red," or "green," is the amount that is shown in that game.
+
+"blue", "red", or "green" WITHOUT the comma means it's the end of the sequence.
+
+Possible: 12 red cubes, 13 green cubes, and 14 blue cubes.
+
+*/
+
+const pos = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green';
+const impos = 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red'
+
+var g = {};
+let reg = /\: (.*)/;
+
+let extraction = pos.match(reg)[1]
+
+let ec = []
+
+ec.push(extraction.match(/^(.*?)\;/)[1])
+
+console.log(ec[0])
+
+/*
+
+3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+
+2 green
+
+3 blue, 4 red; 1 red, 2 green, 6 blue
+
+1 red, 2 green, 6 blue
+
+3 blue, 4 red;
+
+*/
